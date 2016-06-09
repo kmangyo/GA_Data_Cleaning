2d7c7c5813dc2babc6c4d15aca925cdf4898fad1
Hyun Gyo, Kim <khg423@gmail.com>
2016-06-09 18:41:02 +0900

    Update ui.R

--- DAU/ui.R
+++ DAU/ui.R
@@ -1,12 +1,23 @@
 library(shiny)
 library(dplyr)
+library(stringi)
+library(tidyr)
 
 shinyUI(fluidPage(
   fluidRow(
-    fileInput('file1','Choose File',accept=c('.tsv')),
-    selectInput('input', label=h5('Select options'),'', multiple=TRUE, selectize=FALSE),
-    tableOutput("contents"),
+  	titlePanel("GA Data Cleaning"),
+  	sidebarPanel(
+    fileInput('file1','Choose Conv. File',accept=c('.tsv')),
+    fileInput('file2','Choose User File',accept=c('.tsv')),
+    selectInput('input', label=h5('Select options'),'', multiple=TRUE, selectize=FALSE), width = 3),
+    mainPanel(
+    h4("Selected options"),
     verbatimTextOutput('out3'),
-    DT::dataTableOutput("table")
+    h4("Conv."),
+    DT::dataTableOutput("table"),
+    h4("User"),
+    DT::dataTableOutput("table1"),
+    h4("Raw"),
+    tableOutput("contents"))
   )
 ))
